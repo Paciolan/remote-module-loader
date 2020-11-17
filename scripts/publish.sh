4b2c6cbeb702845f651ca36f1d3f8e7492ed9349
Joel Thoms <jthoms@paciolan.com>
2020-11-17 15:45:25 -0800

    fix: 🐛 npm publish with tag

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+if [ -z "$1" ]
+  then
+    echo "ERROR: No argument supplied"
+    exit 1
+fi
+
 node -e "require('fs').writeFileSync('./package.json', JSON.stringify(Object.assign(require('./package.json'), {repository:{type:'git',url:'$GITHUB_URL'}}), null, 2), 'utf8')"
-npm publish
+npm publish --tag $1
